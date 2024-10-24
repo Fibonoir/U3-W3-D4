c4801e63b7202d5f76bc76dca775c40aed26de7e
Valentin Goxhaj <valentinogoxhaj@gmail.com>
2024-10-24 16:43:55 +0200

    updated the signup

--- app/src/app/auth/sign-up/sign-up.component.ts
+++ app/src/app/auth/sign-up/sign-up.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { iUser } from '../auth-interfaces/i-user';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { iLoginRequest } from '../auth-interfaces/i-login-request';
 
 @Component({
   selector: 'app-sign-up',
@@ -9,17 +10,48 @@ import { Router } from '@angular/router';
   styleUrl: './sign-up.component.scss'
 })
 export class SignUpComponent {
-  formData: Partial<iUser> = {}
+  emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  signupForm: FormGroup;
 
   constructor(
     private authSvc: AuthService,
     private router: Router
-  ){}
+  ) {
+    this.signupForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(this.emailPattern)
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6)
+      ]),
+      confirmPassword: new FormControl('', [Validators.required])
+    }, { validators: this.passwordMatchValidator as ValidatorFn });
+  }
 
-  register(){
-    this.authSvc.register(this.formData)
-    .subscribe(res => {
-      this.router.navigate(['/auth/login'])
-    })
+  passwordMatchValidator: ValidatorFn = (form: AbstractControl): ValidationErrors | null => {
+    return form.get('password')?.value === form.get('confirmPassword')?.value
+      ? null : { mismatch: true };
+  }
+
+  signup() {
+    if (this.signupForm.valid) {
+      const signupData: iLoginRequest = this.signupForm.value;
+      this.authSvc.register(signupData).subscribe({
+        next: () => {
+          alert('Signup successful');
+          this.router.navigate(['/auth/login']);
+        },
+        error: (err: any) => {
+          console.error('Signup error', err);
+          alert('An error occurred during signup. Please try again.');
+        }
+      });
+    } else {
+      alert('Invalid signup info. Please check the form.');
+    }
   }
 }
+
